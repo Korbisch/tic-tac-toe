e28a37de9016bca7d2b1e8b39ae6805ddc8808d8
Korbinian Schleifer <korbinian.schleifer@gmail.com>
2023-03-25 16:17:41 +0100

    create tic tac toe game

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,100 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
-function App() {
+const Square: React.FC<{
+  value: string;
+  onSquareClick: () => void;
+}> = ({ value, onSquareClick }) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <button className="square" onClick={onSquareClick}>
+      {value}
+    </button>
+  );
+};
+
+export default function Board() {
+  const initialState = Array(9).fill("");
+  const [squares, setSquares] = useState(initialState);
+  let winner = calculateWinner(squares);
+
+  const makeUsersMove = (i: number, squares: string[]) => {
+    squares[i] = "X";
+  };
+
+  const makeOpponentsMove = (squares: string[]) => {
+    let field = " ";
+    let randomIndex = -1;
+    while (field) {
+      randomIndex = Math.floor(Math.random() * squares.length);
+      field = squares[randomIndex];
+    }
+    squares[randomIndex] = "O";
+  };
+
+  const handleClick = (i: number) => {
+    const squareIsAlreadyFilled = Boolean(squares[i]);
+    if (squareIsAlreadyFilled || winner) {
+      return;
+    }
+    const nextSquares = squares.slice();
+    makeUsersMove(i, nextSquares);
+
+    const boardIsNotFull = Boolean(nextSquares.some((val) => val === ""));
+    winner = calculateWinner(nextSquares);
+
+    if (boardIsNotFull && !winner) {
+      makeOpponentsMove(nextSquares);
+    }
+    setSquares(nextSquares);
+  };
+
+  const resetGame = () => {
+    setSquares(initialState);
+    winner = null;
+  };
+
+  return (
+    <>
+      <div className="status">
+        {winner && (winner === "X" ? "You Win!" : "You Loose!")}
+      </div>
+      <div className="board-row">
+        <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
+        <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
+        <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
+      </div>
+      <div className="board-row">
+        <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
+        <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
+        <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
+      </div>
+      <div className="board-row">
+        <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
+        <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
+        <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
+      </div>
+      <div style={{ marginTop: "10px" }}>
+        <button onClick={resetGame}>Play again!</button>
+      </div>
+    </>
   );
 }
 
-export default App;
+function calculateWinner(squares: string[]) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
